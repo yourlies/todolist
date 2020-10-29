3c2ccc5c6a04a6aad3c61f0c15fca6cee3fd4d7d
lies <958142428@qq.com>
2020-10-29 18:34:09 +0800

    auto save

---
+++ src/components/ToHeader.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="header">
+    <span class="head">TodoList</span>
+    <span class="foot">MTM</span>
+  </div>
+</template>
+<style scoped>
+.header {
+  box-shadow: 1px 1px 3px #888;
+  background-color: #333;
+  color: #fff;
+  margin-bottom: 10px;
+  padding: 10px 20px;
+}
+.header > span {
+  display: inline-block;
+  border-radius: 2px;
+  padding: 3px 5px;
+}
+.header > span.foot {
+  background-color: #f90;
+  color: #000;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,50 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-  </div>
+  <ToContainer>
+    <ToHeader />
+    <div class="home">
+      <div class="comment">“双击新增计划”</div>
+      <div class="comment">“拖动计划，可重新分组”</div>
+      <div class="title">#Date 10/29</div>
+    </div>
+  </ToContainer>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import ToContainer from '@/components/ToContainer.vue';
+import ToHeader from '@/components/ToHeader.vue';
 
 @Options({
-  components: {
-    HelloWorld,
-  },
+  components: { ToHeader, ToContainer },
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  name = 123;
+}
 </script>
+<style scoped>
+.home {
+  padding: 1vw;
+}
+.comment {
+  padding-left: 20px;
+  font-size: 14px;
+}
+.title {
+  position: relative;
+  background-color: #ddd;
+  padding: 10px;
+  padding-left: 20px;
+  margin-top: 10px;
+  border-radius: 3px;
+  color: #333;
+}
+.title:before {
+  position: absolute;
+  content: ' ';
+  border-left: 2px dashed #fff;
+  height: 25px;
+  width: 10px;
+  left: 10px;
+  top: 9px;
+}
+</style>
